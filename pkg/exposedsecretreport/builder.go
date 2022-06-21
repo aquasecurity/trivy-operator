2f01594c5380bca34653d3c7db61d773a7b63213
Jose Donizetti <jdbjunior@gmail.com>
2022-06-21 13:24:24 -0300

    refactor: remove exposed secrets TTL

--- pkg/exposedsecretreport/builder.go
+++ pkg/exposedsecretreport/builder.go
@@ -3,7 +3,6 @@ package exposedsecretreport
 import (
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
 	"github.com/aquasecurity/trivy-operator/pkg/kube"
@@ -22,7 +21,6 @@ type ReportBuilder struct {
 	container  string
 	hash       string
 	data       v1alpha1.ExposedSecretReportData
-	reportTTL  *time.Duration
 }
 
 func NewReportBuilder(scheme *runtime.Scheme) *ReportBuilder {
@@ -48,11 +46,6 @@ func (b *ReportBuilder) PodSpecHash(hash string) *ReportBuilder {
 
 func (b *ReportBuilder) Data(data v1alpha1.ExposedSecretReportData) *ReportBuilder {
 	b.data = data
-	return b
-}
-
-func (b *ReportBuilder) ReportTTL(ttl *time.Duration) *ReportBuilder {
-	b.reportTTL = ttl
 	return b
 }
 
@@ -85,11 +78,6 @@ func (b *ReportBuilder) Get() (v1alpha1.ExposedSecretReport, error) {
 		Report: b.data,
 	}
 
-	if b.reportTTL != nil {
-		report.Annotations = map[string]string{
-			v1alpha1.TTLReportAnnotation: b.reportTTL.String(),
-		}
-	}
 	err := kube.ObjectToObjectMeta(b.controller, &report.ObjectMeta)
 	if err != nil {
 		return v1alpha1.ExposedSecretReport{}, err

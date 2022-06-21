98c36aee41c9cbf9259b3057c4e4b28e5e6a5d9d
Erik Godding Boye <egboye@gmail.com>
2022-06-21 20:01:00 +0200

    fix: re-gen static resources from Helm chart and update doc for doing it

---
+++ hack/update-static.yaml.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+CRD_DIR=$SCRIPT_ROOT/deploy/crd
+HELM_DIR=$SCRIPT_ROOT/deploy/helm
+STATIC_DIR=$SCRIPT_ROOT/deploy/static
+
+HELM_TMPDIR=$(mktemp -d)
+trap "rm -rf $HELM_TMPDIR" EXIT
+
+helm template trivy-operator $HELM_DIR \
+  --namespace trivy-system \
+  --set="managedBy=kubectl" \
+  --output-dir=$TMPDIR/trivy-operator-helm-template
+
+cp $TMPDIR/trivy-operator-helm-template/trivy-operator/templates/rbac.yaml $STATIC_DIR/02-trivy-operator.rbac.yaml
+cp $TMPDIR/trivy-operator-helm-template/trivy-operator/templates/config.yaml $STATIC_DIR/03-trivy-operator.config.yaml
+cp $TMPDIR/trivy-operator-helm-template/trivy-operator/templates/policies.yaml $STATIC_DIR/04-trivy-operator.policies.yaml
+cp $TMPDIR/trivy-operator-helm-template/trivy-operator/templates/deployment.yaml $STATIC_DIR/05-trivy-operator.deployment.yaml
+
+cat $CRD_DIR/vulnerabilityreports.crd.yaml \
+  $CRD_DIR/configauditreports.crd.yaml \
+  $CRD_DIR/exposedsecretreports.crd.yaml \
+  $CRD_DIR/clusterconfigauditreports.crd.yaml \
+  $STATIC_DIR/01-trivy-operator.ns.yaml \
+  $STATIC_DIR/02-trivy-operator.rbac.yaml \
+  $STATIC_DIR/03-trivy-operator.config.yaml \
+  $STATIC_DIR/04-trivy-operator.policies.yaml \
+  $STATIC_DIR/05-trivy-operator.deployment.yaml > $STATIC_DIR/trivy-operator.yaml

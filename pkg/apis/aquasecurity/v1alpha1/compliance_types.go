94e5fc6ae3ea74ec40f0c91e965e778da78c1d95
Erik Godding Boye <egboye@gmail.com>
2022-07-12 16:08:11 +0200

    chore: use controller-gen to generate CRDs from Go markers (#279)
    Signed-off-by: Erik Godding Boye <egboye@gmail.com>

--- pkg/apis/aquasecurity/v1alpha1/compliance_types.go
+++ pkg/apis/aquasecurity/v1alpha1/compliance_types.go
@@ -10,6 +10,11 @@ type ClusterComplianceSummary struct {
 }
 
 //+kubebuilder:object:root=true
+//+kubebuilder:resource:scope=Cluster,shortName={compliance}
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="The age of the report"
+//+kubebuilder:printcolumn:name="Fail",type=integer,JSONPath=`.status.summary.failCount`,priority=1,description="The number of checks that failed with Danger status"
+//+kubebuilder:printcolumn:name="Pass",type=integer,JSONPath=`.status.summary.passCount`,priority=1,description="The number of checks that passed"
 
 // ClusterComplianceReport is a specification for the ClusterComplianceReport resource.
 type ClusterComplianceReport struct {
@@ -21,31 +26,43 @@ type ClusterComplianceReport struct {
 
 //ReportSpec represent the compliance specification
 type ReportSpec struct {
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Cron        string    `json:"cron"`
-	Version     string    `json:"version"`
-	Controls    []Control `json:"controls"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	// cron define the intervals for report generation
+	//+kubebuilder:validation:Pattern=`^(((([\*]{1}){1})|((\*\/){0,1}(([0-9]{1}){1}|(([1-5]{1}){1}([0-9]{1}){1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([0-9]{1}){1}|(([1]{1}){1}([0-9]{1}){1}){1}|([2]{1}){1}([0-3]{1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([1-9]{1}){1}|(([1-2]{1}){1}([0-9]{1}){1}){1}|([3]{1}){1}([0-1]{1}){1}))) ((([\*]{1}){1})|((\*\/){0,1}(([1-9]{1}){1}|(([1-2]{1}){1}([0-9]{1}){1}){1}|([3]{1}){1}([0-1]{1}){1}))|(jan|feb|mar|apr|may|jun|jul|aug|sep|okt|nov|dec)) ((([\*]{1}){1})|((\*\/){0,1}(([0-7]{1}){1}))|(sun|mon|tue|wed|thu|fri|sat)))$`
+	Cron    string `json:"cron"`
+	Version string `json:"version"`
+	// Control represent the cps controls data and mapping checks
+	Controls []Control `json:"controls"`
 }
 
 //Control represent the cps controls data and mapping checks
 type Control struct {
-	ID            string        `json:"id"`
-	Name          string        `json:"name"`
-	Description   string        `json:"description,omitempty"`
-	Kinds         []string      `json:"kinds"`
-	Mapping       Mapping       `json:"mapping"`
-	Severity      Severity      `json:"severity"`
+	// id define the control check id
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description,omitempty"`
+	// kinds define the list of kinds control check apply on, example: Node,Workload
+	Kinds   []string `json:"kinds"`
+	Mapping Mapping  `json:"mapping"`
+	// define the severity of the control
+	//+kubebuilder:validation:Enum={CRITICAL,HIGH,MEDIUM,LOW,UNKNOWN}
+	Severity Severity `json:"severity"`
+	// define the default value for check status in case resource not found
+	//+kubebuilder:validation:Enum={PASS,WARN,FAIL}
 	DefaultStatus ControlStatus `json:"defaultStatus,omitempty"`
 }
 
 //SpecCheck represent the scanner who perform the control check
 type SpecCheck struct {
+	// id define the check id as produced by scanner
 	ID string `json:"id"`
 }
 
 //Mapping represent the scanner who perform the control check
 type Mapping struct {
+	// scanner define the name of the scanner which produce data, currently only config-audit is supported
+	//+kubebuilder:validation:Pattern=`^config-audit$`
 	Scanner string      `json:"scanner"`
 	Checks  []SpecCheck `json:"checks"`
 }
--- pkg/apis/aquasecurity/v1alpha1/exposed_secrets_types.go
+++ pkg/apis/aquasecurity/v1alpha1/exposed_secrets_types.go
@@ -7,15 +7,19 @@ import (
 // ExposedSecretSummary is a summary of ExposedSecret counts grouped by Severity.
 type ExposedSecretSummary struct {
 	// CriticalCount is the number of exposed secrets with Critical Severity.
+	//+kubebuilder:validation:Minimum=0
 	CriticalCount int `json:"criticalCount"`
 
 	// HighCount is the number of exposed secrets with High Severity.
+	//+kubebuilder:validation:Minimum=0
 	HighCount int `json:"highCount"`
 
 	// MediumCount is the number of exposed secrets with Medium Severity.
+	//+kubebuilder:validation:Minimum=0
 	MediumCount int `json:"mediumCount"`
 
 	// LowCount is the number of exposed secrets with Low Severity.
+	//+kubebuilder:validation:Minimum=0
 	LowCount int `json:"lowCount"`
 }
 
@@ -27,15 +31,26 @@ type ExposedSecret struct {
 	// RuleID is rule the identifier.
 	RuleID string `json:"ruleID"`
 
-	Title    string   `json:"title"`
-	Category string   `json:"category"`
+	Title    string `json:"title"`
+	Category string `json:"category"`
+	//+kubebuilder:validation:Enum={CRITICAL,HIGH,MEDIUM,LOW}
 	Severity Severity `json:"severity"`
-	Match    string   `json:"match"`
+	// Match where the exposed rule matched.
+	Match string `json:"match"`
 }
 
 //+kubebuilder:object:root=true
+//+kubebuilder:resource:shortName={exposedsecret,exposedsecrets}
+//+kubebuilder:printcolumn:name="Repository",type=string,JSONPath=`.report.artifact.repository`,description="The name of image repository"
+//+kubebuilder:printcolumn:name="Tag",type=string,JSONPath=`.report.artifact.tag`,description="The name of image tag"
+//+kubebuilder:printcolumn:name="Scanner",type=string,JSONPath=`.report.scanner.name`,description="The name of the exposed secret scanner"
+//+kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="The age of the report"
+//+kubebuilder:printcolumn:name="Critical",type=integer,JSONPath=`.report.summary.criticalCount`,priority=1,description="The number of critical exposed secrets"
+//+kubebuilder:printcolumn:name="High",type=integer,JSONPath=`.report.summary.highCount`,priority=1,description="The number of high exposed secrets"
+//+kubebuilder:printcolumn:name="Medium",type=integer,JSONPath=`.report.summary.mediumCount`,priority=1,description="The number of medium exposed secrets"
+//+kubebuilder:printcolumn:name="Low",type=integer,JSONPath=`.report.summary.lowCount`,priority=1,description="The number of low exposed secrets"
 
-// ExposedSecretReport is a specification for the ExposedSecretReport resource.
+// ExposedSecretReport summarizes exposed secrets in plaintext files built into container images.
 type ExposedSecretReport struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -50,21 +65,25 @@ type ExposedSecretReport struct {
 // @see https://github.com/goharbor/pluggable-scanner-spec/blob/master/api/spec/scanner-adapter-openapi-v1.0.yaml
 type ExposedSecretReportData struct {
 	// UpdateTimestamp is a timestamp representing the server time in UTC when this report was updated.
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Format=date-time
 	UpdateTimestamp metav1.Time `json:"updateTimestamp"`
 
 	// Scanner is the scanner that generated this report.
 	Scanner Scanner `json:"scanner"`
 
 	// Registry is the registry the Artifact was pulled from.
+	//+optional
 	Registry Registry `json:"registry"`
 
-	// Artifact is a container image scanned for exposed secrets.
+	// Artifact represents a standalone, executable package of software that includes everything needed to
+	// run an application.
 	Artifact Artifact `json:"artifact"`
 
-	// Summary is a summary of ExposedSecret counts grouped by Severity.
+	// Summary is the exposed secrets counts grouped by Severity.
 	Summary ExposedSecretSummary `json:"summary"`
 
-	// Secrets is a list of passwords, api keys, tokens and others items found in the Artifact.
+	// Exposed secrets is a list of passwords, api keys, tokens and others items found in the Artifact.
 	Secrets []ExposedSecret `json:"secrets"`
 }
 

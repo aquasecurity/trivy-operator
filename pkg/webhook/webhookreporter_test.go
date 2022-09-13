0624ca3d1f2fe58d277660ba487da86c691d08f9
simar7 <1254783+simar7@users.noreply.github.com>
2022-09-12 23:47:27 -0700

    feat(op): Add support for sending reports to a webhook (#476)
    * feat(op): Adding Webhook reconciler.
    
    Signed-off-by: Simar <simar@linux.com>
    
    * tests(op): Add more testcases for webhookreporter.go
    
    Signed-off-by: Simar <simar@linux.com>
    
    * feat(op): Add sane defaults for deployment yaml.
    
    Signed-off-by: Simar <simar@linux.com>
    
    * fix(op): Add fixes as per PR review.
    
    Signed-off-by: Simar <simar@linux.com>
    
    * fix(op): De-lint
    
    * docs(op): Document the webhook option.
    
    Signed-off-by: Simar <simar@linux.com>
    
    * docs(op): Add `integration` level docs to document usage.
    
    Signed-off-by: Simar <simar@linux.com>
    
    Signed-off-by: Simar <simar@linux.com>

---
+++ pkg/webhook/webhookreporter_test.go
@@ -0,0 +1,91 @@
+package webhook
+
+import (
+	"io"
+	"math"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_sendReports(t *testing.T) {
+	testcases := []struct {
+		name          string
+		want          string
+		inputReport   any
+		timeout       time.Duration
+		expectedError string
+	}{
+		{
+			name: "happy path, vuln report data",
+			want: `{"metadata":{"creationTimestamp":null},"report":{"updateTimestamp":null,"scanner":{"name":"","vendor":"","version":""},"registry":{"server":""},"artifact":{"repository":""},"summary":{"criticalCount":0,"highCount":0,"mediumCount":0,"lowCount":0,"unknownCount":0,"noneCount":0},"vulnerabilities":[{"vulnerabilityID":"CVE-2022-1234","resource":"","installedVersion":"1.2.3","fixedVersion":"3.4.5","severity":"CRITICAL","title":"foo bar very baz","links":null,"target":""}]}}`,
+			inputReport: v1alpha1.VulnerabilityReport{
+				Report: v1alpha1.VulnerabilityReportData{
+					Vulnerabilities: []v1alpha1.Vulnerability{
+						{
+							VulnerabilityID:  "CVE-2022-1234",
+							InstalledVersion: "1.2.3",
+							FixedVersion:     "3.4.5",
+							Severity:         "CRITICAL",
+							Title:            "foo bar very baz",
+						},
+					},
+				},
+			},
+			timeout: time.Hour,
+		},
+		{
+			name: "happy path, secret report data",
+			want: `{"metadata":{"creationTimestamp":null},"report":{"updateTimestamp":null,"scanner":{"name":"","vendor":"","version":""},"registry":{"server":""},"artifact":{"repository":""},"summary":{"criticalCount":0,"highCount":0,"mediumCount":0,"lowCount":0},"secrets":[{"target":"foo bar baz","ruleID":"foo123","title":"bad bad secret","category":"","severity":"CRITICAL","match":""}]}}`,
+			inputReport: v1alpha1.ExposedSecretReport{
+				Report: v1alpha1.ExposedSecretReportData{
+					Secrets: []v1alpha1.ExposedSecret{
+						{
+							Target:   "foo bar baz",
+							RuleID:   "foo123",
+							Title:    "bad bad secret",
+							Severity: "CRITICAL",
+						},
+					},
+				},
+			},
+			timeout: time.Hour,
+		},
+		{
+			name: "sad path, timeout occurs",
+			inputReport: v1alpha1.VulnerabilityReport{
+				Report: v1alpha1.VulnerabilityReportData{},
+			},
+			timeout:       time.Nanosecond,
+			expectedError: "context deadline exceeded (Client.Timeout exceeded while awaiting headers)",
+		},
+		{
+			name:          "sad path, bad report",
+			inputReport:   math.Inf(1),
+			timeout:       time.Hour,
+			expectedError: `failed to marshal reports`,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				b, _ := io.ReadAll(r.Body)
+				assert.JSONEq(t, tc.want, string(b))
+			}))
+			defer ts.Close()
+			gotError := sendReport(tc.inputReport, ts.URL, tc.timeout)
+			switch {
+			case tc.expectedError != "":
+				assert.ErrorContains(t, gotError, tc.expectedError, tc.name)
+			default:
+				require.NoError(t, gotError, tc.name)
+			}
+		})
+	}
+}

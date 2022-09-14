774a32afde9a7a3d4e332115d35f4f418ec54c58
Travis Smith <travis.kenneth.smith@gmail.com>
2022-09-14 17:38:25 -0500

    fix envDefault for TargetWorkloads

--- pkg/operator/etc/config.go
+++ pkg/operator/etc/config.go
@@ -35,7 +35,7 @@ type Config struct {
 	ExposedSecretScannerEnabled                  bool           `env:"OPERATOR_EXPOSED_SECRET_SCANNER_ENABLED" envDefault:"true"`
 	WebhookBroadcastURL                          string         `env:"OPERATOR_WEBHOOK_BROADCAST_URL"`
 	WebhookBroadcastTimeout                      *time.Duration `env:"OPERATOR_WEBHOOK_BROADCAST_TIMEOUT" envDefault:"30s"`
-	TargetWorkloads                              string         `env:"OPERATOR_TARGET_WORKLOADS" envDefault:"Pod,Replicaset,ReplicationController,StatefulSet,DaemonSet,CronJob,Job"`
+	TargetWorkloads                              string         `env:"OPERATOR_TARGET_WORKLOADS" envDefault:"Pod,ReplicaSet,ReplicationController,StatefulSet,DaemonSet,CronJob,Job"`
 }
 
 // GetOperatorConfig loads Config from environment variables.

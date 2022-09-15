57ed70b322d0105db2d2576a751dba0fc1ab4825
Travis Smith <travis.kenneth.smith@gmail.com>
2022-09-15 18:51:10 -0500

    scan all workload resources by default

--- pkg/operator/etc/config.go
+++ pkg/operator/etc/config.go
@@ -35,7 +35,7 @@ type Config struct {
 	ExposedSecretScannerEnabled                  bool           `env:"OPERATOR_EXPOSED_SECRET_SCANNER_ENABLED" envDefault:"true"`
 	WebhookBroadcastURL                          string         `env:"OPERATOR_WEBHOOK_BROADCAST_URL"`
 	WebhookBroadcastTimeout                      *time.Duration `env:"OPERATOR_WEBHOOK_BROADCAST_TIMEOUT" envDefault:"30s"`
-	TargetWorkloads                              string         `env:"OPERATOR_TARGET_WORKLOADS"`
+	TargetWorkloads                              string         `env:"OPERATOR_TARGET_WORKLOADS" envDefault:"Pod,ReplicaSet,ReplicationController,StatefulSet,DaemonSet,CronJob,Job"`
 }
 
 // GetOperatorConfig loads Config from environment variables.
@@ -74,7 +74,7 @@ func (c Config) GetTargetWorkloads() []string {
 		return strings.Split(strings.ToLower(workloads), ",")
 	}
 
-	return []string{}
+	return []string{"pod", "replicaset", "replicationcontroller", "statefulset", "daemonset", "cronjob", "job"}
 }
 
 // InstallMode represents multitenancy support defined by the Operator Lifecycle Manager spec.
--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -44,7 +44,7 @@ func Start(ctx context.Context, buildInfo trivyoperator.BuildInfo, operatorConfi
 		"operator namespace", operatorNamespace,
 		"target namespaces", targetNamespaces,
 		"exclude namespaces", operatorConfig.ExcludeNamespaces,
-		"target workloads", operatorConfig.TargetWorkloads)
+		"target workloads", operatorConfig.GetTargetWorkloads())
 
 	// Set the default manager options.
 	options := manager.Options{

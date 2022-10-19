7f07fcecabdcde509e71f44a1c671668057c61bb
chenk <hen.keinan@gmail.com>
2022-10-19 06:47:27 +0300

    fix: fs system scanning uncompressed report
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/utils/compressutil.go
+++ pkg/utils/compressutil.go
@@ -7,9 +7,9 @@ import (
 	"io"
 )
 
-// DecompressBzip2 accept bzip2 compressed bytes and decompress it
+// decompressBzip2 accept bzip2 compressed bytes and decompress it
 // #nosec
-func DecompressBzip2(compressedBytes []byte) (io.Reader, error) {
+func decompressBzip2(compressedBytes []byte) (io.Reader, error) {
 	bz2Reader := bzip2.NewReader(bytes.NewReader(compressedBytes))
 	uncompressedWriter := new(bytes.Buffer)
 	_, err := io.Copy(uncompressedWriter, bz2Reader)
@@ -19,11 +19,27 @@ func DecompressBzip2(compressedBytes []byte) (io.Reader, error) {
 	return uncompressedWriter, nil
 }
 
-// Base64Decode accept encoded reader and base64 decode it
-func Base64Decode(encodedReader io.Reader) ([]byte, error) {
+// base64Decode accept encoded reader and base64 decode it
+func base64Decode(encodedReader io.Reader) ([]byte, error) {
 	encodedBytes, err := io.ReadAll(encodedReader)
 	if err != nil {
 		return nil, err
 	}
 	return base64.StdEncoding.DecodeString(string(encodedBytes))
+}
+
+func ReadCompressData(encodedReader io.ReadCloser) (io.Reader, error) {
+	buf := &bytes.Buffer{}
+	tee := io.TeeReader(encodedReader, buf)
+	// base64 decode logs
+	compressedLogsBytes, err := base64Decode(tee)
+	if err != nil {
+		return buf, err
+	}
+	// bzip2 decompress logs
+	unCompressedLogsReader, err := decompressBzip2(compressedLogsBytes)
+	if err != nil {
+		return buf, err
+	}
+	return unCompressedLogsReader, nil
 }

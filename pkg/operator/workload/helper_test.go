dc0450e81a73e100c16c5534b32457bc52933fb9
chenk <hen.keinan@gmail.com>
2022-10-23 11:27:26 +0300

    feat: update err message
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/operator/workload/helper_test.go
+++ pkg/operator/workload/helper_test.go
@@ -54,53 +54,6 @@ func TestGetReportsByLabel(t *testing.T) {
 	}
 }
 
-func TestMarkOldReportForImmediateDeletion(t *testing.T) {
-	tests := []struct {
-		name             string
-		reportsPath      string
-		loadResourceFunc func(filePath string, resource interface{}) error
-		loadObjectList   client.ObjectList
-		resourceName     string
-		namespace        string
-		labels           map[string]string
-	}{
-		{name: "get vulnerability report by label", reportsPath: "./testdata/fixture/vulnerabilityReportList.json",
-			loadResourceFunc: loadResource, labels: map[string]string{"trivy-operator.resource.name": "tt-reg"},
-			namespace: "default", resourceName: "pod-tt-reg-test", loadObjectList: &v1alpha1.VulnerabilityReportList{}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := tt.loadResourceFunc(tt.reportsPath, &tt.loadObjectList)
-			assert.NoError(t, err)
-			testClient := fake.NewClientBuilder().WithScheme(trivyoperator.NewScheme()).WithLists(tt.loadObjectList).Build()
-			or := kube.NewObjectResolver(testClient, nil)
-			ctx := context.TODO()
-			err = MarkOldReportForImmediateDeletion(ctx, or, tt.namespace, tt.resourceName)
-			assert.NoError(t, err)
-			switch tt.loadObjectList.(type) {
-			case *v1alpha1.VulnerabilityReportList:
-				updatedReports, err := GetReportsByLabel(context.TODO(), or, &v1alpha1.VulnerabilityReportList{}, tt.namespace, tt.labels)
-				assert.NoError(t, err)
-				for _, up := range updatedReports.Items {
-					up.Annotations["trivy-operator.aquasecurity.github.io/report-ttl"] = "0s"
-				}
-			case *v1alpha1.ConfigAuditReportList:
-				updatedReports, err := GetReportsByLabel(context.TODO(), or, &v1alpha1.ConfigAuditReportList{}, tt.namespace, tt.labels)
-				assert.NoError(t, err)
-				for _, up := range updatedReports.Items {
-					up.Annotations["trivy-operator.aquasecurity.github.io/report-ttl"] = "0s"
-				}
-			case *v1alpha1.ExposedSecretReportList:
-				updatedReports, err := GetReportsByLabel(context.TODO(), or, &v1alpha1.ExposedSecretReportList{}, tt.namespace, tt.labels)
-				assert.NoError(t, err)
-				for _, up := range updatedReports.Items {
-					up.Annotations["trivy-operator.aquasecurity.github.io/report-ttl"] = "0s"
-				}
-			}
-		})
-	}
-}
-
 func loadResource(filePath string, resource interface{}) error {
 	data, err := os.ReadFile(filePath)
 	if err != nil {

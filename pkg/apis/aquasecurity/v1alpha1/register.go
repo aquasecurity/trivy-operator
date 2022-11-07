8e2c8ba8c19eff4eb08682980f89facf7c5bb9c0
chenk <hen.keinan@gmail.com>
2022-11-07 10:19:55 +0200

    feat: support-infra checks
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/apis/aquasecurity/v1alpha1/register.go
+++ pkg/apis/aquasecurity/v1alpha1/register.go
@@ -36,6 +36,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&RbacAssessmentReportList{},
 		&ClusterRbacAssessmentReport{},
 		&ClusterRbacAssessmentReportList{},
+		&InfraAssessmentReport{},
+		&InfraAssessmentReportList{},
 	)
 	meta.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil

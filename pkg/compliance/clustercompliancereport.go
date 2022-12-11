8a7517b2c4bb4c955216bb58fad65ae9cd87986b
chenk <hen.keinan@gmail.com>
2022-12-11 15:32:31 +0200

    test: compliance reconcile
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/compliance/clustercompliancereport.go
+++ pkg/compliance/clustercompliancereport.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
 	"github.com/aquasecurity/trivy-operator/pkg/ext"
+	"github.com/aquasecurity/trivy-operator/pkg/operator/etc"
 	"github.com/aquasecurity/trivy-operator/pkg/utils"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -20,6 +21,7 @@ import (
 type ClusterComplianceReportReconciler struct {
 	logr.Logger
 	client.Client
+	etc.Config
 	Mgr
 	ext.Clock
 }
@@ -60,12 +62,15 @@ func (r *ClusterComplianceReportReconciler) generateComplianceReport(ctx context
 		if err != nil {
 			return fmt.Errorf("failed to check report cron expression %w", err)
 		}
-		if utils.DurationExceeded(durationToNextGeneration) {
+		if utils.DurationExceeded(durationToNextGeneration) || r.Config.InvokeClusterComplianceOnce {
 			err = r.Mgr.GenerateComplianceReport(ctx, report.Spec)
 			if err != nil {
 				log.Error(err, "failed to generate compliance report")
 			}
 			return err
+		}
+		if r.Config.InvokeClusterComplianceOnce { // for demo or testing purposes
+			return nil
 		}
 		log.V(1).Info("RequeueAfter", "durationToNextGeneration", durationToNextGeneration)
 		ctrlResult.RequeueAfter = durationToNextGeneration

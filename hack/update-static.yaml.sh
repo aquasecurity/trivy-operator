06bcb674d912d72f453259c6f8b1c695ca225be8
chenk <hen.keinan@gmail.com>
2022-12-11 09:50:44 +0200

    feat: spec symlink
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- hack/update-static.yaml.sh
+++ hack/update-static.yaml.sh
@@ -19,9 +19,11 @@ cat $CRD_DIR/* > $STATIC_DIR/trivy-operator.yaml
 ## if ns.yaml do not exist, cat namespace.yaml to trivy-operator.yaml (avoid duplicate namespace definition)
 [ ! -f $HELM_TMPDIR/trivy-operator/templates/ns.yaml ] && cat $STATIC_DIR/namespace.yaml >> $STATIC_DIR/trivy-operator.yaml
 
+rm -rf $HELM_TMPDIR/trivy-operator/templates/specs
 cat $HELM_TMPDIR/trivy-operator/templates/* >> $STATIC_DIR/trivy-operator.yaml
 
 # Copy all manifests rendered by the Helm chart to the static resources directory,
 # where they should be ignored by Git.
 # This is done to support local development with partial updates to local cluster.
 cp $HELM_TMPDIR/trivy-operator/templates/* $STATIC_DIR/
+

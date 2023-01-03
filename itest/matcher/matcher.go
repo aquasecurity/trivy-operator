2f72722a1da1656bc49e1b8257f2edd200ed03f6
chenk <hen.keinan@gmail.com>
2023-01-03 09:53:59 +0200

    chore: bump trivy v0.36.0 (#828)
    * chore: bump trivy v0.36.0
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump trivy v0.36.0
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- itest/matcher/matcher.go
+++ itest/matcher/matcher.go
@@ -21,7 +21,7 @@ var (
 	trivyScanner = v1alpha1.Scanner{
 		Name:    v1alpha1.ScannerNameTrivy,
 		Vendor:  "Aqua Security",
-		Version: "0.35.0",
+		Version: "0.36.0",
 	}
 	builtInScanner = v1alpha1.Scanner{
 		Name:    v1alpha1.ScannerNameTrivy,

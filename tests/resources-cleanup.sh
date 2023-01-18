2680eeedff5a9e499af3990e21ebe5a40e43f3ae
chenk <hen.keinan@gmail.com>
2023-01-18 11:33:42 +0200

    test: client-server mode (#858)
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- tests/resources-cleanup.sh
+++ tests/resources-cleanup.sh
@@ -6,3 +6,4 @@ kubectl delete crd clusterconfigauditreports.aquasecurity.github.io
 kubectl delete crd rbacassessmentreports.aquasecurity.github.io
 kubectl delete crd infraassessmentreports.aquasecurity.github.io
 kubectl delete crd clusterrbacassessmentreports.aquasecurity.github.io
+kubectl delete crd clustercompliancereports.aquasecurity.github.io

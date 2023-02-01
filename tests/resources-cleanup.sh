e39bcba0ec24ef409dd8c2b586ab4a561e90ad3f
chenk <hen.keinan@gmail.com>
2023-02-01 16:53:13 +0200

    test: update resource clean-up
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- tests/resources-cleanup.sh
+++ tests/resources-cleanup.sh
@@ -6,6 +6,3 @@ kubectl delete crd clusterconfigauditreports.aquasecurity.github.io
 kubectl delete crd rbacassessmentreports.aquasecurity.github.io
 kubectl delete crd infraassessmentreports.aquasecurity.github.io
 kubectl delete crd clusterrbacassessmentreports.aquasecurity.github.io
-kubectl delete crd clustercompliancereports.aquasecurity.github.io
-kubectl delete crd clusterinfraassessmentreports.aquasecurity.github.io
-

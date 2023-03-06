e0f47eb65adfde417498491f047895fd1fa084b9
chenk <hen.keinan@gmail.com>
2023-03-06 13:27:05 +0200

    fix: requeue non-ready nodes
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/configauditreport/controller/node.go
+++ pkg/configauditreport/controller/node.go
@@ -67,7 +67,15 @@ func (r *NodeReconciler) reconcileNodes() reconcile.Func {
 			}
 			return ctrl.Result{}, fmt.Errorf("getting node from cache: %w", err)
 		}
-
+		// requeue nodes non-ready nodes
+		for _, condition := range node.Status.Conditions {
+			if condition.Type == corev1.NodeReady {
+				if !(condition.Status == corev1.ConditionTrue) {
+					log.V(1).Info("Pushing back node collector job", "retryAfter", r.ScanJobRetryAfter)
+					return ctrl.Result{RequeueAfter: r.Config.ScanJobRetryAfter}, nil
+				}
+			}
+		}
 		log.V(1).Info("Checking whether cluster Infra assessments report exists")
 		hasReport, err := hasInfraReport(ctx, node, r.InfraReadWriter)
 		if err != nil {
@@ -99,15 +107,6 @@ func (r *NodeReconciler) reconcileNodes() reconcile.Func {
 		if limitExceeded {
 			log.V(1).Info("Pushing back node collector job", "count", jobsCount, "retryAfter", r.ScanJobRetryAfter)
 			return ctrl.Result{RequeueAfter: r.Config.ScanJobRetryAfter}, nil
-		}
-		// requeue nodes non-ready nodes
-		for _, condition := range node.Status.Conditions {
-			if condition.Type == corev1.NodeReady {
-				if !(condition.Status == corev1.ConditionTrue) {
-					log.V(1).Info("Pushing back node collector job", "count", jobsCount, "retryAfter", r.ScanJobRetryAfter)
-					return ctrl.Result{RequeueAfter: r.Config.ScanJobRetryAfter}, nil
-				}
-			}
 		}
 		cluster, err := k8s.GetCluster()
 		if err != nil {

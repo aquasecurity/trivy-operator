de4b63efa45f673d154579644e94db9b6780e7dd
chenk <hen.keinan@gmail.com>
2023-06-06 12:11:04 +0300

    chore: bump trivy v0.42.0 (#1265)
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/plugins/trivy/plugin.go
+++ pkg/plugins/trivy/plugin.go
@@ -544,7 +544,7 @@ func (p *plugin) Init(ctx trivyoperator.PluginContext) error {
 	return ctx.EnsureConfig(trivyoperator.PluginConfig{
 		Data: map[string]string{
 			keyTrivyImageRepository:           DefaultImageRepository,
-			keyTrivyImageTag:                  "0.41.0",
+			keyTrivyImageTag:                  "0.42.0",
 			KeyTrivySeverity:                  DefaultSeverity,
 			keyTrivySlow:                      "true",
 			keyTrivyMode:                      string(Standalone),

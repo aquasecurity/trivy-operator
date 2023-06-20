24d97ea4b0f6eead522118e7545985316370b109
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-06-20 16:35:13 +0100

    fixing failed unit test

--- pkg/metrics/collector.go
+++ pkg/metrics/collector.go
@@ -728,13 +728,11 @@ func (c *ResourcesMetricsCollector) collectRbacAssessmentInfoReports(ctx context
 					labelValues[5] = rbac.Title
 					labelValues[6] = rbac.Description
 					labelValues[7] = rbac.Category
-					labelValues[8] = NewSeverityLabel(rbac.Severity).Label
-
 					for i, label := range c.GetReportResourceLabels() {
 						labelValues[i+9] = r.Labels[label]
 					}
-					metrics <- prometheus.MustNewConstMetric(c.rbacAssessmentInfoDesc, prometheus.GaugeValue, float64(1), labelValues...)
-					//c.populateRbacAssessmentValues(labelValues, c.rbacAssessmentInfoDesc, r.Report.Summary, metrics, 8)
+
+					c.populateRbacAssessmentValues(labelValues, c.rbacAssessmentInfoDesc, r.Report.Summary, metrics, 8)
 				}
 			}
 		}
@@ -809,6 +807,9 @@ func (c *ResourcesMetricsCollector) populateRbacAssessmentValues(labelValues []s
 	for severity, countFn := range c.rbacAssessmentSeverities {
 		labelValues[index] = severity
 		count := countFn(summary)
+		if c.Config.MetricsRbacAssessmentInfo {
+			count = 1
+		}
 		metrics <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, float64(count), labelValues...)
 
 	}

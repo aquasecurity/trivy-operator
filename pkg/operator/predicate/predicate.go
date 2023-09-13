50e84a67191e604a3781453dd341a70908a0d834
chenk <hen.keinan@gmail.com>
2023-09-13 14:09:12 +0300

    chore: bump controller-runtime-0.16.1 (#1505)
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    * chore: bump controller-runtime-0.16.1
    
    Signed-off-by: chenk <hen.keinan@gmail.com>
    
    ---------
    
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/operator/predicate/predicate.go
+++ pkg/operator/predicate/predicate.go
@@ -46,17 +46,15 @@ var InstallModePredicate = func(config etc.Config) (predicate.Predicate, error) 
 		if mode == etc.AllNamespaces && strings.TrimSpace(config.ExcludeNamespaces) != "" {
 			namespaces := strings.Split(config.ExcludeNamespaces, ",")
 			for _, namespace := range namespaces {
-				matches, err := filepath.Match(strings.TrimSpace(namespace), obj.GetNamespace())
+				matched, err := filepath.Match(strings.TrimSpace(namespace), obj.GetNamespace())
 				if err != nil {
-					// In case of error we'd assume the resource should be scanned
 					return true
 				}
-				if matches {
+				if matched {
 					return false
 				}
 			}
 		}
-
 		return true
 	}), nil
 }

5f5a8402f7b07cbbc02164fc9f2d7a892c909f46
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-10-26 22:33:05 +0100

    Added clusterCompliance_info metrics

--- pkg/metrics/collector_test.go
+++ pkg/metrics/collector_test.go
@@ -830,8 +830,8 @@ var _ = Describe("ResourcesMetricsCollector", func() {
 						Name: "car1 cluster compliance name",
 					},
 				}...)
-			car1.Status.Summary.FailCount = 12
-			car1.Status.Summary.PassCount = 15
+			car1.Status.Summary.FailCount = 1
+			car1.Status.Summary.PassCount = 1
 
 			client.WithRuntimeObjects(car1)
 		})

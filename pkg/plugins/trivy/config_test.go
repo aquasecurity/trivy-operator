b5a6c1525390c5c30b2dcb3955275d64d2f0b8a1
chenk <hen.keinan@gmail.com>
2024-01-10 12:35:35 +0200

    chore: bump trivy v0.48.2
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/plugins/trivy/config_test.go
+++ pkg/plugins/trivy/config_test.go
@@ -729,7 +729,7 @@ func TestPlugin_Init(t *testing.T) {
 			},
 			Data: map[string]string{
 				"trivy.repository":                DefaultImageRepository,
-				"trivy.tag":                       "0.48.1",
+				"trivy.tag":                       "0.48.2",
 				"trivy.severity":                  DefaultSeverity,
 				"trivy.slow":                      "true",
 				"trivy.mode":                      string(Standalone),

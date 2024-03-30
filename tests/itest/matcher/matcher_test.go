540f0dfcd08a667830692ef75682a1f7ddf3dc20
chenk <hen.keinan@gmail.com>
2024-03-30 17:05:34 +0300

    refactor: tests e2e config
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- tests/itest/matcher/matcher_test.go
+++ tests/itest/matcher/matcher_test.go
@@ -5,9 +5,9 @@ import (
 
 	"testing"
 
-	"github.com/aquasecurity/trivy-operator/tests/itest/matcher"
 	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
 	"github.com/aquasecurity/trivy-operator/pkg/trivyoperator"
+	"github.com/aquasecurity/trivy-operator/tests/itest/matcher"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"

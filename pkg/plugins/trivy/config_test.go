7141bcfc7154f1f6aa065c80e847f9f90f936a4a
Mark Dalton Gray <graymark@microsoft.com>
2025-02-10 14:38:25 -0600

    init Support Additional Metadata + CVSS Score Logic Update

--- pkg/plugins/trivy/config_test.go
+++ pkg/plugins/trivy/config_test.go
@@ -95,10 +95,10 @@ func TestConfig_GetAdditionalVulnerabilityReportFields(t *testing.T) {
 			name: "all additional fields are set",
 			configData: Config{PluginConfig: trivyoperator.PluginConfig{
 				Data: map[string]string{
-					"trivy.additionalVulnerabilityReportFields": "PackageType,PkgPath,Class,Target,Links,Description,CVSS",
+					"trivy.additionalVulnerabilityReportFields": "PackageType,PkgPath,Class,Target,Links,Description,CVSS,SeveritySource,DataSource",
 				},
 			}},
-			additionalFields: vulnerabilityreport.AdditionalFields{Description: true, Links: true, CVSS: true, Class: true, PackageType: true, PkgPath: true, Target: true},
+			additionalFields: vulnerabilityreport.AdditionalFields{Description: true, Links: true, CVSS: true, Class: true, PackageType: true, PkgPath: true, Target: true, SeveritySource: true, DataSource: true},
 		},
 		{
 			name: "some additional fields are set",
@@ -120,6 +120,8 @@ func TestConfig_GetAdditionalVulnerabilityReportFields(t *testing.T) {
 			assert.Equal(t, tc.additionalFields.PackageType, addFields.PackageType)
 			assert.Equal(t, tc.additionalFields.Class, addFields.Class)
 			assert.Equal(t, tc.additionalFields.Links, addFields.Links)
+			assert.True(t, addFields.SeveritySource == tc.additionalFields.SeveritySource)
+			assert.True(t, addFields.DataSource == tc.additionalFields.DataSource)
 		})
 	}
 }

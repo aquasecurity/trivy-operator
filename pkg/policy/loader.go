d7ae64a10a9967b698eccc87670e12ce488b726a
Simar <simar@linux.com>
2025-04-02 23:49:16 -0600

    fix signature

--- pkg/policy/loader.go
+++ pkg/policy/loader.go
@@ -116,7 +116,10 @@ func (pl *policyLoader) getBuiltInPolicies(ctx context.Context) ([]string, error
 	if err = client.DownloadBuiltinChecks(ctx, pl.RegistryOptions); err != nil {
 		return nil, xerrors.Errorf("failed to download built-in policies: %w", err)
 	}
-	return client.LoadBuiltinChecks()
+	var paths []string
+	paths = append(paths, client.LoadBuiltinChecks())
+
+	return paths, nil
 }
 
 func LoadPoliciesData(policyPath []string) ([]string, error) {

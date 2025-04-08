5d60ec9e3205b951fd5e9b6f75f7fcd9d2c2d642
simar7 <1254783+simar7@users.noreply.github.com>
2025-04-08 00:27:29 -0600

    chore(deps): Bump `trivy-*` deps (#2507)
    * chore(deps): Bump trivy-* deps
    
    * fix signature
    
    * update checks
    
    * cleanup returns

--- pkg/policy/loader.go
+++ pkg/policy/loader.go
@@ -116,7 +116,8 @@ func (pl *policyLoader) getBuiltInPolicies(ctx context.Context) ([]string, error
 	if err = client.DownloadBuiltinChecks(ctx, pl.RegistryOptions); err != nil {
 		return nil, xerrors.Errorf("failed to download built-in policies: %w", err)
 	}
-	return client.LoadBuiltinChecks()
+
+	return []string{client.LoadBuiltinChecks()}, nil
 }
 
 func LoadPoliciesData(policyPath []string) ([]string, error) {

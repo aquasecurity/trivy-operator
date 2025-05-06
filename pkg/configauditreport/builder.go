3800278822111b92c1a468c2fd1e815a4f375317
mleykin <mleykin@squarespace.com>
2025-05-06 16:01:35 -0500

    fixing up spacing

--- pkg/configauditreport/builder.go
+++ pkg/configauditreport/builder.go
@@ -2,22 +2,22 @@ package configauditreport
 
 import (
 	"context"
-  "context"
-  "fmt"
-  "strings"
-  "time"
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/apimachinery/pkg/labels"
-  "k8s.io/apimachinery/pkg/runtime"
-  "k8s.io/apimachinery/pkg/util/validation"
-  "k8s.io/utils/ptr"
-  "sigs.k8s.io/controller-runtime/pkg/client"
-  "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"fmt"
+	"strings"
+	"time"
 
-  "github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
-  "github.com/aquasecurity/trivy-operator/pkg/kube"
-  "github.com/aquasecurity/trivy-operator/pkg/operator/etc"
-  "github.com/aquasecurity/trivy-operator/pkg/trivyoperator"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
+	"github.com/aquasecurity/trivy-operator/pkg/kube"
+	"github.com/aquasecurity/trivy-operator/pkg/operator/etc"
+	"github.com/aquasecurity/trivy-operator/pkg/trivyoperator"
 )
 
 type ReportBuilder struct {

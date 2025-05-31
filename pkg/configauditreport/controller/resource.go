e32ac064b799df971b0a8ebd14ea84eca83a8cec
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-05-31 11:34:21 +0200

    fix: enable octalLiteral rule from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- pkg/configauditreport/controller/resource.go
+++ pkg/configauditreport/controller/resource.go
@@ -306,15 +306,15 @@ func (r *ResourceController) writeAlternateReports(resource client.Object, misCo
 		infraAssessmentDir := filepath.Join(reportDir, "infra_assessment_reports")
 
 		// Ensure the directories exist
-		if err := os.MkdirAll(configAuditDir, 0750); err != nil {
+		if err := os.MkdirAll(configAuditDir, 0o750); err != nil {
 			log.Error(err, "Failed to create configAuditDir")
 			return ctrl.Result{}, err
 		}
-		if err := os.MkdirAll(rbacAssessmentDir, 0750); err != nil {
+		if err := os.MkdirAll(rbacAssessmentDir, 0o750); err != nil {
 			log.Error(err, "Failed to create rbacAssessmentDir")
 			return ctrl.Result{}, err
 		}
-		if err := os.MkdirAll(infraAssessmentDir, 0750); err != nil {
+		if err := os.MkdirAll(infraAssessmentDir, 0o750); err != nil {
 			log.Error(err, "Failed to create infraAssessmentDir")
 			return ctrl.Result{}, err
 		}
@@ -328,7 +328,7 @@ func (r *ResourceController) writeAlternateReports(resource client.Object, misCo
 			return ctrl.Result{}, err
 		}
 		configReportPath := filepath.Join(configAuditDir, fmt.Sprintf("%s-%s.json", workloadKind, workloadName))
-		err = os.WriteFile(configReportPath, configReportData, 0600)
+		err = os.WriteFile(configReportPath, configReportData, 0o600)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
@@ -340,7 +340,7 @@ func (r *ResourceController) writeAlternateReports(resource client.Object, misCo
 			return ctrl.Result{}, err
 		}
 		infraReportPath := filepath.Join(infraAssessmentDir, fmt.Sprintf("%s-%s.json", workloadKind, workloadName))
-		err = os.WriteFile(infraReportPath, infraReportData, 0600)
+		err = os.WriteFile(infraReportPath, infraReportData, 0o600)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
@@ -352,7 +352,7 @@ func (r *ResourceController) writeAlternateReports(resource client.Object, misCo
 			return ctrl.Result{}, err
 		}
 		rbacReportPath := filepath.Join(rbacAssessmentDir, fmt.Sprintf("%s-%s.json", workloadKind, workloadName))
-		err = os.WriteFile(rbacReportPath, rbacReportData, 0600)
+		err = os.WriteFile(rbacReportPath, rbacReportData, 0o600)
 		if err != nil {
 			return ctrl.Result{}, err
 		}

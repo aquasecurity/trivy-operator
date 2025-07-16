be0f59db5b31aa57c6b15dc59925bbd9144fdb1c
afdesk <work@afdesk.com>
2025-07-16 10:45:39 +0600

    chore: bump up Trivy to version 0.64.1 (#2654)
    * chore: bump up Trivy to version 0.64.1
    
    * fix: correct version for test case
    
    * fix: correct generated README

--- pkg/plugins/trivy/plugin_test.go
+++ pkg/plugins/trivy/plugin_test.go
@@ -6369,7 +6369,7 @@ default ignore = false`,
 			},
 			config: map[string]string{
 				"trivy.repository":                "docker.io/aquasec/trivy",
-				"trivy.tag":                       "0.63.0",
+				"trivy.tag":                       "0.64.1",
 				"trivy.mode":                      string(trivy.Standalone),
 				"trivy.dbRepository":              trivy.DefaultDBRepository,
 				"trivy.javaDbRepository":          trivy.DefaultJavaDBRepository,
@@ -6426,7 +6426,7 @@ default ignore = false`,
 				InitContainers: []corev1.Container{
 					{
 						Name:                     "00000000-0000-0000-0000-000000000001",
-						Image:                    "docker.io/aquasec/trivy:0.63.0",
+						Image:                    "docker.io/aquasec/trivy:0.64.1",
 						ImagePullPolicy:          corev1.PullIfNotPresent,
 						TerminationMessagePolicy: corev1.TerminationMessageFallbackToLogsOnError,
 						Env: []corev1.EnvVar{
@@ -6518,7 +6518,7 @@ default ignore = false`,
 				Containers: []corev1.Container{
 					{
 						Name:                     "nginx",
-						Image:                    "docker.io/aquasec/trivy:0.63.0",
+						Image:                    "docker.io/aquasec/trivy:0.64.1",
 						ImagePullPolicy:          corev1.PullIfNotPresent,
 						TerminationMessagePolicy: corev1.TerminationMessageFallbackToLogsOnError,
 						Env: []corev1.EnvVar{

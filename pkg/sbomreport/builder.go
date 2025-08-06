2acb3db723daf735f4f1b2b55b4a8afddf5d2684
afdesk <work@afdesk.com>
2025-08-07 05:52:01 +0600

    chore: migrate to aws-go v2 (#2694)

--- pkg/sbomreport/builder.go
+++ pkg/sbomreport/builder.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	containerimage "github.com/google/go-containerregistry/pkg/name"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"

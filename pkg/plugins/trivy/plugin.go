0ec40483800c5c4349030119bfec74599a104bd1
afdesk <work@afdesk.com>
2025-12-04 13:27:56 +0600

    chore: bump up Trivy to version v0.68.1

--- pkg/plugins/trivy/plugin.go
+++ pkg/plugins/trivy/plugin.go
@@ -80,7 +80,7 @@ func (p *plugin) Init(ctx trivyoperator.PluginContext) error {
 	return ctx.EnsureConfig(trivyoperator.PluginConfig{
 		Data: map[string]string{
 			keyTrivyImageRepository:           DefaultImageRepository,
-			keyTrivyImageTag:                  "0.67.0",
+			keyTrivyImageTag:                  "0.68.1",
 			KeyTrivySeverity:                  DefaultSeverity,
 			keyTrivySlow:                      "true",
 			keyTrivyMode:                      string(Standalone),

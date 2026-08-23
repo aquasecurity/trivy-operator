bf45e672d046567782995d28d27dfde7729f1523
Chiman Jain <chimanjain15@gmail.com>
2026-08-23 11:06:22 +0530

    chore: upgrade caarlos0/env from v6 to v11

--- tests/itest/helper/helper.go
+++ tests/itest/helper/helper.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/caarlos0/env/v6"
+	"github.com/caarlos0/env/v11"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
